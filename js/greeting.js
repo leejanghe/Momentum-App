836e934022a28542c565c264d3c9f1bf8bf9e14c
leejanghe <babybye777@gmail.com>
2022-01-20 03:11:15 +0900

    login js done

---
+++ js/greeting.js
@@ -0,0 +1,26 @@
+const writeForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const h1 = document.querySelector('#greeting');
+
+
+function handleWriteName(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    writeForm.style.display = 'none';
+    localStorage.setItem("username", username);
+    greeting(username);
+}
+
+function greeting(username){
+    h1.innerText = `Welcome ${username}`
+    h1.classList.remove('hide')
+}
+
+const savedUsername = localStorage.getItem('username')
+
+if(savedUsername === null){
+    writeForm.addEventListener('submit',handleWriteName)
+}else{
+    greeting(savedUsername)
+    writeForm.style.display = 'none';
+}
