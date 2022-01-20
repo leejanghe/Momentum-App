55d177cc73f6eb9fa00d0fabbfcb2153204242c3
leejanghe <babybye777@gmail.com>
2022-01-20 12:08:30 +0900

    clock 년도 요일 추가

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,22 @@
 const clock = document.querySelector('#clock');
+const dataPrint = document.querySelector('#dataPrint span:first-child');
+const timePrint = document.querySelector('#timePrint span:last-child');
+
 
 function currentTime(){
     const data = new Date();
+    const weeks = ['SUN','MON','TUE','WEB','THU','FRI','SAT'];
+    const years = data.getFullYear();
+    const months = data.getMonth()+1;
+    const days = data.getDate();
+    const week = weeks[data.getDay()];
+
     const hours =  String(data.getHours()).padStart(2,"0")
     const minutes = String(data.getMinutes()).padStart(2,"0")
     const seconds = String(data.getSeconds()).padStart(2,"0")
-    clock.innerText = `${hours}:${minutes}:${seconds}`
+
+    dataPrint.innerText = `${years}.${months}.${days}.${week}`;
+    timePrint.innerText = `${hours}:${minutes}:${seconds}`
 }
 currentTime()
 setInterval(currentTime, 1000);
